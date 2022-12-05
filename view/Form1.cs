37d8e5d9cd5475f3fdb7fc4366de1e7e5416f21b
minjuuH <hwangmj1124@gmail.com>
2022-12-06 02:00:44 +0900

    검색 기능 연결
    - Form1.cs 파일 수정
    - 수월한 검색을 위해 Code 필드 문자열형으로 변경
    - textbox에 아무값을 입력하지 않은 채 검색 버튼을 클릭할 경우 전체 데이터가 출력되도록 수정
    - 전체 데이터를 저장할 멤버 필드 지정

--- view/Form1.cs
+++ view/Form1.cs
@@ -14,6 +14,10 @@ namespace n1
     {
         DBClass dbclass = new DBClass();
 
+        //데이터테이블 멤버
+        DataTable wholeinoutTable;   //입출고 내역
+        DataTable wholeitemTable;     //전체재고
+
         public Form1()
         {
             InitializeComponent();
@@ -27,26 +31,27 @@ namespace n1
             this.dataGridView1.Visible = true;
             this.dataGridView2.Visible = false;
 
-            DataTable inoutTable = dbclass.LoadDT("select * from warehousing");
-            DataTable allTable = dbclass.LoadDT("select * from item_info");
-            inoutTable.Columns.RemoveAt(0);
+            wholeinoutTable = dbclass.LoadDT("select * from warehousing");
+            wholeitemTable = dbclass.LoadDT("select * from item_info");
+            wholeinoutTable.Columns.RemoveAt(0);
 
-            foreach (DataRow dr in inoutTable.Rows)
-            {
-                if (dr["In_out"].ToString() == "1")
-                    dr["In_out"] = "입고";
-                else if (dr["In_out"].ToString() == "0")
-                    dr["In_out"] = "출고";
-            }
+            //foreach (DataRow dr in inoutTable.Rows)
+            //{
+            //    if (dr["In_out"].ToString() == "1")
+            //        dr["In_out"] = "입고";
+            //    else if (dr["In_out"].ToString() == "0")
+            //        dr["In_out"] = "출고";
+            //}
 
             //dataGridView1.Columns.Clear();    //그리드뷰 columns을 다 비워주는 코드
 
             //입출고 데이터
-            dataGridView1.DataSource = inoutTable;
+            dataGridView1.DataSource = wholeinoutTable;
 
             //전체 재고 데이터            
-            dataGridView2.DataSource = allTable;
+            dataGridView2.DataSource = wholeitemTable;
         }
+
         //입출고내역 버튼 클릭시
         private void button2_Click(object sender, EventArgs e)
         {
@@ -55,6 +60,7 @@ namespace n1
             this.dataGridView1.Visible = true;
             this.dataGridView2.Visible = false;
         }
+
         //전체 재고 버튼 클릭시
         private void button3_Click(object sender, EventArgs e)
         {
@@ -64,17 +70,34 @@ namespace n1
             this.dataGridView2.Visible = true;
             this.dataGridView2.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
         }
+
         //등록 버튼 
         private void button1_Click(object sender, EventArgs e)
         {
             MessageBox.Show("등록할 내용을 입력하세요.", "등록 오류", MessageBoxButtons.OK, MessageBoxIcon.Warning);
         }
+
         //검색 버튼
         private void button4_Click(object sender, EventArgs e)
         {
             if (String.IsNullOrEmpty(textBox9.Text))
             {
-                MessageBox.Show("검색할 내용을 입력하세요.", "검색 오류", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                //MessageBox.Show("검색할 내용을 입력하세요.", "검색 오류", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                dataGridView1.DataSource = wholeinoutTable;
+                dataGridView2.DataSource = wholeitemTable;
+            }
+            else if(dataGridView1.Visible)
+            {
+                string searchtext = "%" + textBox9.Text + "%";
+                DataTable inoutTable = dbclass.LoadDT(String.Format("SELECT * FROM warehousing Where Code LIKE '{0}' OR Item LIKE '{1}'", searchtext, searchtext));
+                inoutTable.Columns.RemoveAt(0);
+                dataGridView1.DataSource = inoutTable;
+            }
+            else if (dataGridView2.Visible)
+            {
+                string searchtext = "%" + textBox9.Text + "%";
+                DataTable itemTable = dbclass.LoadDT(String.Format("SELECT * FROM item_info Where Code LIKE '{0}' OR Item LIKE '{1}'", searchtext, searchtext));
+                dataGridView2.DataSource = itemTable;
             }
         }
     }
