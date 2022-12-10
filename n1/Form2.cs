55c9c16de9d1c53e528a91ac066b060888a274b8
minjuuH <hwangmj1124@gmail.com>
2022-12-10 14:53:12 +0900

    기타 파일 변경

---
+++ n1/Form2.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace n1
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // 검색하면 해당 내용 그리드 뷰에 띄움
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Form3 form3 = new Form3();
+            form3.ShowDialog();
+        }
+    }
+}
